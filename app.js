d2b083e1a232deba6d1fc31451e8ce3a12b09b43
Jonathan Zacsh <jzacsh@gmail.com>
2011-06-05 13:14:58 -0400

    starting over, with clean slate. using express.js instead of doing stuff from scratch.

--- app.js
+++
@@ -1,6 +0,0 @@
-var server = require('./server.js');
-var router = require('./router.js').router;
-var options = require('./options.js');
-
-server.start(router, options);
-console.log('server started successfully');
