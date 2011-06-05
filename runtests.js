5b3ac615744d07998b982db99b74153aae82656c
Jonathan Zacsh <jzacsh@gmail.com>
2011-06-05 13:23:01 -0400

    hoping to learn things a nice and proper tdd way.

---
+++ runtests.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+//taken from http://stella.laurenzo.org/2011/03/bulletproof-node-js-coding/
+
+
+//  require.paths.unshift(__dirname + 'app.js');
+//  process.chdir(__dirname);
