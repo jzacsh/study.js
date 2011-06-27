0f40f1f5238ba920bbb5b80ca9f1ff7009c302a9
Jonathan Zacsh <jzacsh@gmail.com>
2011-06-26 23:01:02 -0400

    simpler solution to seperateed "link" perception.

--- public/scripts/site-ui.js
+++ public/scripts/site-ui.js
@@ -1,12 +1,2 @@
-StudySite.run.linkifyStacks = function(context) {
-  $('#stacks ul#stack-listing li:not(pseudo-link)', context)
-    .each(function() {
-      $(this).click(function() {
-        $(this).children('a').not('.clicked')
-          .addClass('clicked')
-          .trigger('click');
-      });
-    })
-    .addClass('pseudo-link');
-}
+StudySite.run.linkifyStacks = function(context) { }
 
