70bccf1720a3f47beda76cca9be9ee502fd23777
Jonathan Zacsh <jzacsh@gmail.com>
2011-06-30 00:40:11 -0400

    getting jquery UI involved for easy of use.

--- public/scripts/site-ui.js
+++ public/scripts/site-ui.js
@@ -1,2 +1,21 @@
-StudySite.run.linkifyStacks = function(context) { }
+StudySite.run.collapseApi = function(context) {
+  var $api = $('#api.section', context);
+  var $h2 = $('h2', $api);
+
+  $h2.click(function() {
+    $('.content', $api).toggle('slide');
+    if ($h2.hasClass('expandable')) {
+      $h2.removeClass('expandable');
+      $h2.addClass('collapsable');
+    }
+    else {
+      $h2.addClass('expandable');
+      $h2.removeClass('collapsable');
+    }
+    console.log($('.content', $api).attr('style'));
+  });
+}
+
+// .expandable
+// .collapsable
 
